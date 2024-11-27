fc7a359ffbd10f8fa6f7e4e0425395e91391537c
jmac1 <145376972+JMacPort@users.noreply.github.com>
2024-11-27 12:45:21 -0500

    I2C implementation added. LCD init next

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -2,6 +2,20 @@
 #include <stdio.h>
 #include <string.h>
 
+// Definitions
+#define LCD_ADDR  			0x27			// LCD Address
+#define LCD_EN 	  			0x04			// Enable
+#define LCD_RW    			0x02			// Read/Write
+#define LCD_RS    			0x01			// Register Select
+#define LCD_BL	  			0x08			// Back-light
+// LCD Commands
+#define LCD_CLEAR 			0x01			// Clear Screen
+#define LCD_HOME  			0x02			// Cursor Home
+#define LCD_ENTRY_MODE  	0x06			// Increments cursor, no shift
+#define LCD_DISPLAY_ON  	0x0C			// Display on, cursor off, blink off
+#define LCD_FUNCTION_SET 	0x28			// 4-bit mode, 2 lines, 5x8 font
+
+
 // Prototypes
 void USART_Init();
 
@@ -10,9 +24,66 @@ int main () {
 
 
 	while (1) {
-		printf("Hello!\r\n");
-		for (volatile int i = 0; i < 10000000; i++);
+
 	}
+}
+
+void I2C_Init() {
+	RCC -> APB1ENR |= (1 << 21);									// I2C1 Clock
+
+	I2C1 -> CR1 |= (1 << 15);										// I2C1 Restart
+	I2C1 -> CR1 &= ~(1 << 15);
+
+	I2C1 -> CR2 |= (42 << 0);										// Frequency = 42
+	I2C1 -> CCR = 210;												// Communication Speed = 210
+	I2C1 -> TRISE = 43;												// Rise Time = 43
+
+	I2C1 -> CR1 |= (1 << 0);										// I2C1 Enable
+}
+
+// Checks if bus is free
+int I2C_Check_Busy() {
+	if (I2C1 -> SR2 & (1 << 1)) {
+		return 1;
+	}
+	return 0;
+}
+
+// Generates start bit and waits for generation to complete
+void I2C_Start() {
+	I2C1 -> CR1 |= (1 << 8);
+	while (!(I2C1 -> SR1 & (1 << 0)));
+}
+
+// Sends device address for communication
+void I2C_Send_Address(uint8_t address, uint8_t read) {
+	I2C1 -> DR |= (address << 1) | read;
+	while(!(I2C1 -> SR1 & (1 << 1)));
+
+	uint8_t temp = I2C1 -> SR1;
+	temp = I2C1 -> SR2;
+	(void) temp;
+}
+
+// Sends a byte of data to the device
+void I2C_Send_Data(uint8_t data) {
+	while(!(I2C1 -> DR & (1 << 7)));
+	I2C1 -> DR = data;
+	while(!(I2C1 -> SR1 & (1 << 2)));
+}
+
+// Generates stop condition
+void I2C_Stop() {
+	I2C1 -> CR1 |= (1 << 9);
+}
+
+// Complete I2C Cycle; Writes a byte of data to a device
+void I2C_Write(uint8_t address, uint8_t data) {
+	while(I2C_Check_Busy());
+	I2C_Start();
+	I2C_Send_Address(address, 0);
+	I2C_Send_Data(data);
+	I2C_Stop();
 }
 
 void USART_Init() {
@@ -30,7 +101,8 @@ void USART_Init() {
 	USART2 -> CR1 |= (1 << 13);										// USART2 Enabled
 }
 
-int __io_putchar(int c) {											// printf() retargetting
+// printf() retargeting
+int __io_putchar(int c) {
 	while(!(USART2 -> SR & (1 << 7)));
 	USART2 -> DR = c;
 
