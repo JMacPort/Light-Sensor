c8fdc5675d33c9e150b6d4e434a2354d2cae6563
jmac1 <145376972+JMacPort@users.noreply.github.com>
2024-11-27 16:12:33 -0500

    Working LCD display for strings and data

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,11 +1,12 @@
 #include "main.h"
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
 // Definitions
+// LCD
 #define LCD_ADDR  			0x27			// LCD Address
 #define LCD_EN 	  			0x04			// Enable
-#define LCD_RW    			0x02			// Read/Write
 #define LCD_RS    			0x01			// Register Select
 #define LCD_BL	  			0x08			// Back-light
 // LCD Commands
@@ -14,15 +15,26 @@
 #define LCD_ENTRY_MODE  	0x06			// Increments cursor, no shift
 #define LCD_DISPLAY_ON  	0x0C			// Display on, cursor off, blink off
 #define LCD_FUNCTION_SET 	0x28			// 4-bit mode, 2 lines, 5x8 font
+// Light Sensor
+#define Light_ADDR			0x23			// Light Sensor Address
 
 
 // Prototypes
 void USART_Init();
+void I2C_Init();
+void LCD_Init();
+void LCD_Send_Command(uint8_t);
+void LCD_Send_Data(uint8_t);
+void LCD_Send_String(char*);
+//void Find_Addr();
+
 
 int main () {
 	USART_Init();
+	I2C_Init();
+	LCD_Init();
 
-
+	LCD_Send_String("Hello");
 	while (1) {
 
 	}
@@ -30,6 +42,16 @@ int main () {
 
 void I2C_Init() {
 	RCC -> APB1ENR |= (1 << 21);									// I2C1 Clock
+	RCC -> AHB1ENR |= (1 << 1);										// GPIOB Clock
+
+	GPIOB -> MODER &= ~((3 << (2 * 8)) | (3 << (2 * 9)));			// PB8 & 9 to Alternate Function
+	GPIOB -> MODER |= (2 << (2 * 8)) | (2 << (2 * 9));
+
+	GPIOB -> OTYPER |= (1 << 8) | (1 << 9);							// Open-Drain on PB8 & 9
+
+	GPIOB -> PUPDR |= (1 << (2 * 8)) | (1 << (2 * 9));				// Pull-Up Resistors
+
+	GPIOB -> AFR[1] |= (4 << 0) | (4 << 4);							// Alternate Function to I2C1
 
 	I2C1 -> CR1 |= (1 << 15);										// I2C1 Restart
 	I2C1 -> CR1 &= ~(1 << 15);
@@ -56,18 +78,23 @@ void I2C_Start() {
 }
 
 // Sends device address for communication
-void I2C_Send_Address(uint8_t address, uint8_t read) {
-	I2C1 -> DR |= (address << 1) | read;
+int I2C_Send_Address(uint8_t address, uint8_t read) {
+	I2C1 -> DR = (address << 1) | read;
+	for (volatile int i = 0; i < 5000; i++);
+	if (I2C1 -> SR1 & (1 << 10)) {
+		return 0;
+	}
 	while(!(I2C1 -> SR1 & (1 << 1)));
 
 	uint8_t temp = I2C1 -> SR1;
 	temp = I2C1 -> SR2;
 	(void) temp;
+	return 1;
 }
 
 // Sends a byte of data to the device
 void I2C_Send_Data(uint8_t data) {
-	while(!(I2C1 -> DR & (1 << 7)));
+	while(!(I2C1 -> SR1 & (1 << 7)));
 	I2C1 -> DR = data;
 	while(!(I2C1 -> SR1 & (1 << 2)));
 }
@@ -84,6 +111,44 @@ void I2C_Write(uint8_t address, uint8_t data) {
 	I2C_Send_Address(address, 0);
 	I2C_Send_Data(data);
 	I2C_Stop();
+}
+
+void LCD_Send_Command(uint8_t cmd) {
+	uint8_t upper = (cmd & 0xF0) | LCD_BL;
+	uint8_t lower = ((cmd << 4) & 0xF0) | LCD_BL;
+
+	I2C_Write(LCD_ADDR, upper | LCD_EN);
+	I2C_Write(LCD_ADDR, upper);
+
+	I2C_Write(LCD_ADDR, lower | LCD_EN);
+	I2C_Write(LCD_ADDR, lower);
+}
+
+void LCD_Init() {
+	LCD_Send_Command(0x33);
+	LCD_Send_Command(0x32);
+	LCD_Send_Command(LCD_FUNCTION_SET);
+	LCD_Send_Command(LCD_ENTRY_MODE);
+	LCD_Send_Command(LCD_DISPLAY_ON);
+	LCD_Send_Command(LCD_CLEAR);
+}
+
+void LCD_Send_Data(uint8_t data) {
+	uint8_t upper = (data & 0xF0) | LCD_BL | LCD_RS;
+	uint8_t lower = ((data << 4) & 0xF0) | LCD_BL | LCD_RS;
+
+	I2C_Write(LCD_ADDR, upper | LCD_EN);
+	I2C_Write(LCD_ADDR, upper);
+
+	I2C_Write(LCD_ADDR, lower | LCD_EN);
+	I2C_Write(LCD_ADDR, lower);
+}
+
+void LCD_Send_String(char* str) {
+	while (*str != '\0' ) {
+		LCD_Send_Data(*str);
+		str++;
+	}
 }
 
 void USART_Init() {
@@ -110,3 +175,25 @@ int __io_putchar(int c) {
 	USART2 -> SR &= ~(1 << 6);
 	return c;
 }
+
+// Used to discover device addresses - No longer necessary
+//void Find_Addr() {
+//    printf("Starting scan...\r\n");
+//
+//    for (int i = 0; i < 128; i++) {
+//        I2C_Start();
+//
+//        I2C1->DR = (i << 1) | 0;
+//
+//        for (volatile int j = 0; j < 50000; j++);
+//
+//        if (!(I2C1->SR1 & (1 << 10))) {
+//            printf("Device found at: 0x%02X\r\n", i);
+//        }
+//
+//        I2C1->SR1 &= ~(1 << 10);
+//        I2C_Stop();
+//
+//        for (volatile int j = 0; j < 10000; j++);
+//    }
+//}
